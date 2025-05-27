ff80bf2c44cc5f521354890980c6b2d8bbff757c
rismamutia <rismamutiadewi89@gmail.com>
2025-05-27 19:07:37 +0700

    membuat program utama dari class dokter dan class pasien

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -31,5 +31,49 @@ class dokter {
             cout << "Dokter \"" << nama
             << "\" tidak ada\n";
         }
+
+        void tambahPasien(pasien*);
+        void cetakPasien();
+};
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+
+void pasien::cetakDokter() {
+    cout << "Daftar dokter yang menangani pasien \""
+    << this->nama << "\":\n";
+    //auto digunakan dalam perulangan
+    for (auto& a : daftar_dokter) {
+        cout << a->nama <<"\n";
+    }
+    cout << endl;
+}
+
+int main() {
+    dokter* varDokter1 = new dokter("dr.Budi");
+    dokter* varDokter2 = new dokter("dr.Tono");
+    pasien* varPasien1 = new pasien("Andi");
+    pasien* varPasien2 = new pasien("Lia");
+   
+    varDokter1->tambahPasien(varPasien1);
+    varDokter1->tambahPasien(varPasien2);
+    varDokter2->tambahPasien(varPasien1);
+
+    varPasien1->tambahDokter(varDokter1);
+    varPasien2->tambahDokter(varDokter1);
+    varPasien1->tambahDokter(varDokter2);
+
+    varDokter1->cetakPasien();
+    varDokter2->cetakPasien();
+    varPasien1->cetakDokter();
+    varPasien2->cetakDokter();
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varPasien1;
+    delete varPasien2;
+
+    return 0;
 }
 
