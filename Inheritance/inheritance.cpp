2476a594013b265331f15810dfb29fe6582540f4
rismamutia <rismamutiadewi89@gmail.com>
2025-05-27 20:08:31 +0700

    mendeklarasikan class jantung

--- Inheritance/inheritance.cpp
+++ Inheritance/inheritance.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class orang {
     public:
         string nama;
+        
         orang(string pNama) :
             nama(pNama){
                 cout << "orang dibuat\n" << endl;
@@ -13,4 +14,30 @@ class orang {
                 cout << "orang dihapus\n" << endl;
             }
             
+            int jumlah(int a, int b) {
+                return a + b;
+            }
+
+};
+
+class pelajar : public orang {
+    public:
+    string sekolah;
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "Pelajar dihapus\n" << endl;
+    }
+    string perkenalan(){
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n"}
+    
+};
+
+int main(){
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10,90) << endl;
+
+    return 0;
 }
